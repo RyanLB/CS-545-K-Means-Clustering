426cff66bf0e166e2ffea9bd9b22c83462978039
Ryan Bernstein <lbryanlb@comcast.net>
2016-03-02 15:15:44 -0800

    Initial Commit

---
+++ K-Means Clustering/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  K-Means Clustering
+//
+//  Created by Ryan Bernstein on 3/2/16.
+//  Copyright © 2016 Ryan Bernstein. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
