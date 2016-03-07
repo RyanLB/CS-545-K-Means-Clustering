bf7d48920447f738fcec2648181a951a21c0b345
Ryan Bernstein <lbryanlb@comcast.net>
2016-03-06 17:44:18 -0800

    Add lazy calculation/memoization of numeric properties to KMTestResult

--- K-Means Clustering/Model/KMTestResult.swift
+++ K-Means Clustering/Model/KMTestResult.swift
@@ -13,33 +13,58 @@ struct KMTestResult {
     private let buckets: [[NumberInstance]]
     private let guesses: [Int]
     
+    // Lazy calculation of numeric result properties
+    private var _accuracy: Double?
+    private var _sumSquaredError: Double?
+    private var _sumSquaredDistance: Double?
+    
     init(clusters: ClusterSet, buckets: [[NumberInstance]], guesses: [Int]) {
         self.clusters = clusters
         self.buckets = buckets
         self.guesses = guesses
+        
+        _accuracy = nil
+        _sumSquaredError = nil
+        _sumSquaredDistance = nil
+        
         assert(clusters.clusters.count == buckets.count && buckets.count == guesses.count)
     }
     
     var accuracy: Double {
-        get {
-            let bucketsAndGuesses = zip(buckets, guesses).map{ ($0.0, forGuess: $0.1) }
-            let clusterAccuracies = zip(clusters.clusters, bucketsAndGuesses).map{ $0.0.accuracyFromData($0.1) }
+        mutating get {
+            if _accuracy == nil {
             
-            let total = buckets.map{ $0.count }.reduce(0, combine: +)
-            let weights = buckets.map{ Double($0.count) / Double(total) }
+                let bucketsAndGuesses = zip(buckets, guesses).map{ ($0.0, forGuess: $0.1) }
+                let clusterAccuracies = zip(clusters.clusters, bucketsAndGuesses).map{ $0.0.accuracyFromData($0.1) }
+                
+                let total = buckets.map{ $0.count }.reduce(0, combine: +)
+                let weights = buckets.map{ Double($0.count) / Double(total) }
+                
+                _accuracy = zip(clusterAccuracies, weights).map{ $0.0 * $0.1 }.reduce(0, combine: +)
+            }
             
-            return zip(clusterAccuracies, weights).map{ $0.0 * $0.1 }.reduce(0, combine: +)
+            return _accuracy!
         }
     }
     
     var sumSquaredError: Double {
-        get {
-            return clusters.sumSquaredErrorWithBuckets(buckets)
+        mutating get {
+            if _sumSquaredError == nil {
+                _sumSquaredError = clusters.sumSquaredErrorWithBuckets(buckets)
+            }
+            
+            return _sumSquaredError!
         }
     }
     
     var sumSquaredDistance: Double {
-        return clusters.sumSquaredSeparation()
+        mutating get {
+            if _sumSquaredDistance == nil {
+                _sumSquaredDistance = clusters.sumSquaredSeparation()
+            }
+            
+            return _sumSquaredDistance!
+        }
     }
     
 }
